4108f5a173e8c88c936b2e5b0627e478c1d28fa6
Capomagico <matty.cap@live.it>
2024-03-07 17:07:04 +0100

    edit1

---
+++ src/components/CardRistorante.svelte
@@ -0,0 +1,66 @@
+<script>
+	export let immagine;
+	export let prezzo;
+	export let piatto;
+	export let ristorante;
+</script>
+
+<div class="card_ristorante">
+	<img class="card_ristorante__img" src={immagine} alt="Copertina del piatto" />
+
+	<div class="card_ristorante__desc">
+		<p class="card_ristorante__prezzo">{prezzo}</p>
+		<p class="card_ristorante__piatto">{piatto}</p>
+	</div>
+
+	<div class="card_ristorante__footer">
+		<p>{ristorante}</p>
+	</div>
+</div>
+
+<style>
+	.card_ristorante {
+		margin-left: 20px;
+		background-color: lightgray;
+		width: 300px;
+		color: black;
+		font-family: Montserrat;
+		margin-top: 20px;
+	}
+
+	.card_ristorante__img {
+		aspect-ratio: 1;
+		background-color: red;
+		object-fit: cover;
+		width: 100%;
+		margin-bottom: 5px;
+	}
+
+	.card_ristorante__desc {
+		padding-left: 8px;
+		width: 100%;
+		margin-top: 15px;
+	}
+
+	.card_ristorante__prezzo {
+		font-size: 14px;
+		font-weight: 300;
+		margin-bottom: -8px;
+	}
+	.card_ristorante__piatto {
+		font-size: 24px;
+		font-weight: 700;
+	}
+
+	.card_ristorante__footer {
+		background-color: black;
+		font-weight: 500;
+		font-style: italic;
+		color: white;
+		font-size: 15px;
+		padding-top: 5px;
+		padding-bottom: 5px;
+		display: flex;
+		justify-content: center;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,40 @@
 <script>
-	import CardMusica from '$components/CardMusica.svelte';
+	import CardRistorante from '$components/CardRistorante.svelte';
 
-	let disco_1 = {
-		artista: 'John cena',
-		disco: 'Ciao',
-		durata: '10:20',
-		anno: '1230',
-		image:
-			'https://images.unsplash.com/photo-1709632237343-4583d7f96f52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8'
-	};
+	let risto = [
+		{
+			immagine:
+				'https://plus.unsplash.com/premium_photo-1663858367001-89e5c92d1e0e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D',
+			prezzo: '12€',
+			piatto: 'sadasdasda',
+			ristorante: 'sasasd'
+		},
+		{
+			immagine:
+				'https://plus.unsplash.com/premium_photo-1676106623769-539ecc6d7f92?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZvb2R8ZW58MHx8MHx8fDA%3D',
+			prezzo: '15€',
+			piatto: 'asdasuajdadas',
+			ristorante: 'sfafasfas'
+		},
+		{
+			immagine:
+				'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFtYnVyZ2VyfGVufDB8fDB8fHww',
+			prezzo: '11€',
+			piatto: 'jdajncsascx',
+			ristorante: 'dasfafvc'
+		}
+	];
 </script>
 
 <svelte:head>
 	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
 </svelte:head>
 
-<CardMusica
-	artista="John cena"
-	disco="Ciao"
-	durata="10:20"
-	anno="113"
-	image="https://images.unsplash.com/photo-1709632237343-4583d7f96f52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8"
-/>
-
-<CardMusica
-	artista="OOK"
-	disco="Altro nome"
-	durata="11:24"
-	image="https://plus.unsplash.com/premium_photo-1709311897767-f6ce7e1fc227?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
-/>
+{#each risto as risto}
+	<CardRistorante
+		immagine={risto.immagine}
+		prezzo={risto.prezzo}
+		piatto={risto.piatto}
+		ristorante={risto.ristorante}
+	/>
+{/each}
